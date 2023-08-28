2299251109266273feb59e1f17edef93d1077c02
Ree9o <ree9.business@gmail.com>
2023-08-28 10:05:25 +0900

    migration修正：商品一覧、詳細機能実装

--- db/migrate/20230825111333_add_table_constraint.rb
+++ db/migrate/20230825111333_add_table_constraint.rb
@@ -2,7 +2,16 @@
 
 class AddTableConstraint < ActiveRecord::Migration[7.0]
   def up
-    change_table :items, bulk: true
+    change_column :items, :name, :string, null: false
+    change_column :items, :price, :integer, null: false
+    change_column :items, :content, :text, null: false
     change_column :items, :sale_badge, :boolean, default: false
+  end
+
+  def down
+    change_column :items, :name, :string, null: true
+    change_column :items, :price, :integer, null: true
+    change_column :items, :content, :text, null: true
+    change_column :items, :sale_badge, :boolean, default: nil
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_20_124833) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_111333) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_20_124833) do
     t.boolean "sale_badge", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "tasks", force: :cascade do |t|
